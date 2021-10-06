f4f27f3d8d77b8f699499a56ccd60af7e757cf15
Wang Tao <wangt@esquel.com>
2021-10-06 16:14:47 +0800

    fix: add InventoryLevelService init

--- goshopify.go
+++ goshopify.go
@@ -290,6 +290,7 @@ func NewClient(app App, shopName, token string, opts ...Option) *Client {
 	c.InventoryItem = &InventoryItemServiceOp{client: c}
 	c.ShippingZone = &ShippingZoneServiceOp{client: c}
 	c.ProductListing = &ProductListingServiceOp{client: c}
+	c.InventoryLevel = &InventoryLevelServiceOp{client: c}
 
 	// apply any options
 	for _, opt := range opts {
